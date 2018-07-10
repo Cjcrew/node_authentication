906851c21045954fbd267c6f117fa388758a691e
Cjcrew <cj_crew@aol.com>
2018-07-10 17:16:08 +0300

    first commit

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// model
+const User = mongoose.model('User', new mongoose.Schema({
+  username:   {type: String, required: true},
+  firstName:  {type: String, required: true},
+  lastName:   {type: String, required: true},
+  email:      {type: String, required: true, unique: true},
+  password:   {type: String, required: true}
+}));
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,55 @@
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const mongoose    = require('mongoose');
+const app         = express();
+const User        = require('./models/user');
+
+//APP CONFIG
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+//CONNECT DB
+mongoose.connect('mongodb://localhost/auth');
+mongoose.connection.once('open', () => {
+  console.log('Connection to db has been made...');
+}).on('error', () => {
+  console.error.bind(console, 'Connection error:');
+});
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+});
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard.ejs')
+});
+
+app.post('/register', (req, res) => {
+  let user = new User(req.body);
+
+  user.save((err) => {
+    if (err) {
+      let error = "Something bad happened! Please try again.";
+      if (err.code === 11000) {
+        error = "That email is already taken, please try another.";
+      }
+
+      return res.render('register', {error: error});
+    }
+
+    res.redirect('/dashboard');
+  });
+});
+
+app.listen(3000, () => console.log('App started on port 3000...'));
