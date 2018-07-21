6138f144095b914b97ee055074b6a9de7fe5cb3a
Cjcrew <cj_crew@aol.com>
2018-07-22 01:07:19 +0300

    Refactor middleware

--- middleware/login_required.js
+++ middleware/login_required.js
@@ -1,10 +1,20 @@
 const express     = require('express');
+const middlewareObj = {};
 
-function loginRequired(req, res, next) {
+middlewareObj.loginRequired = (req, res, next) => {
   if (!req.user) {
     return res.redirect('/login');
+  } else {
+    next();
   }
-  next();
 }
 
-module.exports = loginRequired;
+middlewareObj.ifLoggedIn = (req, res, next) => {
+  if (!req.user) {
+    next();
+  } else {
+    return res.redirect('/dashboard');
+  }
+}
+
+module.exports = middlewareObj;
