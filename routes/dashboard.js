f090833b454def8a910238c3ed21f03b294bd8b6
Cjcrew <cj_crew@aol.com>
2018-07-21 16:51:00 +0300

    Implement user auth middleware

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,28 +1,15 @@
-const express     = require('express');
-const mongoose    = require('mongoose');
-const router      = express.Router();
-const sessions    = require('client-sessions');
-const User        = require('../models/user');
+const express       = require('express');
+const mongoose      = require('mongoose');
+const router        = express.Router();
+const sessions      = require('client-sessions');
+const User          = require('../models/user');
+const loginRequired = require('../middleware/login_required');
 
 // =======================
 //      DASHBOARD ROUTE
 // =======================
-router.get('/', (req, res, next) => {
-  if (!(req.session && req.session.userId)) {
-    return res.redirect('/login');
-  }
-
-  User.findById(req.session.userId, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-
-    else if (!user) {
-      return res.redirect('/login');
-    }
-
-    res.render('dashboard.ejs');
-  });
+router.get('/', loginRequired, (req, res, next) => {
+  res.render('dashboard.ejs');
 });
 
 module.exports = router;
