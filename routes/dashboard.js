6efaa760f5e5dcbce33093f6d0d6fcd9af58097d
Cjcrew <cj_crew@aol.com>
2018-07-23 00:16:22 +0300

    Improve comments

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,15 @@
+// ===================================================
 const express       = require('express');
 const mongoose      = require('mongoose');
 const router        = express.Router();
 const sessions      = require('client-sessions');
 const User          = require('../models/user');
-const middleware = require('../middleware/login_required');
+const middleware    = require('../middleware/login_required');
+// ===================================================
+
 
 // =======================
-// DASHBOARD ROUTE
+// DASHBOARD
 // =======================
 router.get('/', middleware.loginRequired, (req, res, next) => {
   res.render('dashboard.ejs', { csrfToken: req.csrfToken() });
