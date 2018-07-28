c9046eb389a6c415af34533613a6ce9ac47dcfa9
Cjcrew <cj_crew@aol.com>
2018-07-28 17:49:54 +0300

    Create js scripts dir & add dashboard script

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+// Grab the Modal
+var modal = document.getElementById('id01');
+
+//  When the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
